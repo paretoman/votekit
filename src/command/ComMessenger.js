1f35828f76715f260537786a10a3cde34e4dbcb4
paretoman <paretoman1@gmail.com>
2022-04-08 20:27:49 -0400

    Make dist build better. Make dist look like src; copy html and css files to dist. Remove .babelrc and remove private properties in classes in order to do so.

--- src/command/ComMessenger.js
+++ src/command/ComMessenger.js
@@ -4,67 +4,64 @@
  * This is kind of like a mediator pattern, but also maybe an observer pattern. */
 export default class ComMessenger {
     /** Make sure broadcasted commands don't trigger broadcasts */
-    #protect
 
-    #commanders
-
-    #linked
+    // All variables are intended to be private.
 
     constructor() {
-        this.#linked = true
-        this.#commanders = []
-        this.#protect = false
+        this.linked = true
+        this.commanders = []
+        this.protect = false
     }
 
     setLinked(value) {
-        this.#linked = value
+        this.linked = value
     }
 
     addCommander(commander) {
-        this.#commanders.push(commander)
+        this.commanders.push(commander)
     }
 
     broadcastDo(command, originCommander) {
-        if (this.#linked && this.#protect === false) {
-            this.#protect = true
-            this.#commanders.forEach((com) => com.passDo(command))
-            this.#protect = false
+        if (this.linked && this.protect === false) {
+            this.protect = true
+            this.commanders.forEach((com) => com.passDo(command))
+            this.protect = false
         } else {
             originCommander.passDo(command)
         }
     }
 
     broadcastDoCommands(commands, originCommander) {
-        if (this.#linked && this.#protect === false) {
-            this.#protect = true
-            this.#commanders.forEach(
+        if (this.linked && this.protect === false) {
+            this.protect = true
+            this.commanders.forEach(
                 (com) => com.passDoCommands(commands),
             )
-            this.#protect = false
+            this.protect = false
         } else {
             originCommander.passDoCommands(commands)
         }
     }
 
     broadcastLoadConfig(newConfig, originCommander) {
-        if (this.#linked && this.#protect === false) {
-            this.#protect = true
-            this.#commanders.forEach(
+        if (this.linked && this.protect === false) {
+            this.protect = true
+            this.commanders.forEach(
                 (com) => com.passLoadConfig(newConfig),
             )
-            this.#protect = false
+            this.protect = false
         } else {
             originCommander.passLoadConfig(newConfig)
         }
     }
 
     broadCastLoadCommands(commands, originCommander) {
-        if (this.#linked && this.#protect === false) {
-            this.#protect = true
-            this.#commanders.forEach(
+        if (this.linked && this.protect === false) {
+            this.protect = true
+            this.commanders.forEach(
                 (com) => com.passLoadCommands(commands),
             )
-            this.#protect = false
+            this.protect = false
         } else {
             originCommander.passLoadCommands(commands)
         }
