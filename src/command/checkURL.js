1fd4f3ac45e9806790c6eac2aa3f14fa058694aa
paretoman <paretoman1@gmail.com>
2022-04-24 17:41:33 -0400

    Fix URL decoding so colors decode correctly.

--- src/command/checkURL.js
+++ src/command/checkURL.js
@@ -5,11 +5,21 @@
  * @returns {Object} - Property yes means we have a config to use from the URL.
  */
 export default function checkURL() {
-    const urlParams = new URLSearchParams(window.location.search)
+    const url = window.location.search
+    const urlParams = new URLSearchParams(url)
 
     const yes = urlParams.has('a')
 
-    const config = yes ? JSON.parse(urlParams.get('a')) : {}
+    const config = yes ? JSON.parse(getParameterByName('a', url)) : {}
 
     return { yes, config }
+}
+
+function getParameterByName(name, url) {
+    const name2 = name.replace(/[[\]]/g, '\\$&')
+    const regex = new RegExp(`[?&]${name2}(=([^&#]*)|&|#|$)`)
+    const results = regex.exec(url)
+    if (!results) return null
+    if (!results[2]) return ''
+    return decodeURIComponent(results[2].replace(/\+/g, ' ')).replace('}/', '}') // not sure how that / got there.
 }
