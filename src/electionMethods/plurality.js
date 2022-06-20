ea0dd7d9fa293b3a8a1190e6fb7ce9d07d7b6a37
paretoman <paretoman1@gmail.com>
2022-06-20 12:21:49 -0400

    Docs for plurality.

--- src/electionMethods/plurality.js
+++ src/electionMethods/plurality.js
@@ -1,14 +1,16 @@
 /** @module */
 
+import { maxIndex } from '../utilities/jsHelpers.js'
+
 /**
  * the candidate with the highest tally wins
  * @param {Object} votes
- * @param {Number[]} votes.tallyFractions
+ * @param {Number[]} votes.tallyFractions - A list of fractions of voters
+ * who chose a candidate, indexed by candidate.
  * @returns {socialChoiceResults}
  */
 export default function plurality(votes) {
-    const max = Math.max(...votes.tallyFractions)
-    const iWinner = votes.tallyFractions.indexOf(max)
+    const iWinner = maxIndex(votes.tallyFractions)
 
     const socialChoiceResults = { iWinner }
     return socialChoiceResults
