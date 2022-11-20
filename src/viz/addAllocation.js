2e21b124b8795d4a42cd0044c93c13db76fb5073
paretoman <paretoman1@gmail.com>
2022-11-20 11:18:56 -0500

    Small Change. Cleanup addAllocation.

--- src/viz/addAllocation.js
+++ src/viz/addAllocation.js
@@ -1,9 +1,17 @@
+/** @module */
+
+/**
+ * When there is just one winner,
+ * sometimes the allocation isn't included in the election results.
+ * So we fill the gap, for now.
+ * @param {Object} electionResults
+ * @returns {Object} electionResults
+ */
 export default function addAllocation(electionResults) {
-    const { allocation } = electionResults
-    const { votes } = electionResults
+    const { allocation, votes } = electionResults
     const { tallyFractions } = votes
     if (allocation === undefined) {
-        const nk = votes.tallyFractions.length
+        const nk = tallyFractions.length
         const wins = Array(nk).fill(0)
         wins[electionResults.iWinner] = 1
 
