paretoman/votekit

commit 097e4ca0948043fa0de8ea1346341ab828a2b052

Use graphic x and y instead of entity x and y. The view has graphics. The model has entities. Also, darkmode line color is partially fixed.

src/sim/states/ViewOne.js
@@ -72,9 +72,9 @@ export default function ViewOne(screen, menu, changes, sim, view) {
         sim.candidateList.canButton.show()
         vizOne.enter()
         vizExplanation.enter()
-        voterViewList.updateXY()
-        candidateViewList.updateXY()
-        view.voterTest.updateXY()
+        voterViewList.updateViewXY()
+        candidateViewList.updateViewXY()
+        view.voterTest.updateViewXY()
     }
 
     self.exit = () => {
@@ -92,7 +92,6 @@ export default function ViewOne(screen, menu, changes, sim, view) {
         vizOne.update(electionResults)
         vizExplanation.update(electionResults)
         self.testVoteView()
-        changes.clear()
 
         screen.clear()
         screen.clearMaps()

src/sim/states/ViewSample.js
@@ -51,8 +51,8 @@ export default function ViewSample(screen, menu, changes, sim, view) {
         superEnter()
         sim.candidateDnList.canDnButton.show()
         enterStrategy()
-        voterViewList.updateXY()
-        candidateDnViewList.updateXY()
+        voterViewList.updateViewXY()
+        candidateDnViewList.updateViewXY()
     }
 
     self.exit = () => {

src/candidateDns/CandidateDn.js
@@ -71,18 +71,10 @@ export default function CandidateDistribution(
     self.setAction.shape2p = (p) => {
         self.shape2.x = p.x
         self.shape2.y = p.y
-        if (sim.election.dimensions === 2) {
-            self.x = p.x
-            self.y = p.y
-        }
         changes.add(['draggables'])
     }
     self.setAction.shape1x = (p) => {
         self.shape1.x = p
-        if (sim.election.dimensions === 1) {
-            self.x = p
-            self.y = 250
-        }
         changes.add(['draggables'])
     }
     self.setXY = (p) => {
@@ -92,15 +84,6 @@ export default function CandidateDistribution(
         } else {
             const cur = candidateDnCommander.setForListSenders.shape2p.getCurrentValue(id)
             candidateDnCommander.setForListSenders.shape2p.go(id, p, cur)
-        }
-    }
-    /** Do this when entering a state because x and y change.
-     *  Maybe x and y should be in the SimCandidateDn instead... just speculating. */
-    self.updateXY = () => {
-        if (sim.election.dimensions === 1) {
-            self.setAction.shape1x(self.shape1.x)
-        } else {
-            self.setAction.shape2p({ x: self.shape2.x, y: self.shape2.y })
         }
     }
 

src/candidateDns/CandidateDnViewList.js
@@ -58,8 +58,8 @@ export default function CandidateDnViewList(view, sim, changes, screen, election
             })
         }
     }
-    self.updateXY = () => {
-        canDnViews.forEach((canDnView) => canDnView.canDn.updateXY())
+    self.updateViewXY = () => {
+        canDnViews.forEach((canDnView) => canDnView.graphic.updateViewXY())
     }
 
     // Render //

src/candidates/Candidate.js
@@ -69,18 +69,10 @@ export default function Candidate(
     self.setAction.shape2p = (p) => {
         self.shape2.x = p.x
         self.shape2.y = p.y
-        if (sim.election.dimensions === 2) {
-            self.x = p.x
-            self.y = p.y
-        }
         changes.add(['draggables'])
     }
     self.setAction.shape1x = (p) => {
         self.shape1.x = p
-        if (sim.election.dimensions === 1) {
-            self.x = p
-            self.y = 250
-        }
         changes.add(['draggables'])
     }
     self.setXY = (p) => {
@@ -90,15 +82,6 @@ export default function Candidate(
         } else {
             const cur = candidateCommander.setForListSenders.shape2p.getCurrentValue(id)
             candidateCommander.setForListSenders.shape2p.go(id, p, cur)
-        }
-    }
-    /** Do this when entering a state because x and y change.
-     *  Maybe x and y should be in the CandidateView instead... just speculating. */
-    self.updateXY = () => {
-        if (sim.election.dimensions === 1) {
-            self.setAction.shape1x(self.shape1.x)
-        } else {
-            self.setAction.shape2p({ x: self.shape2.x, y: self.shape2.y })
         }
     }
 

src/candidates/CandidateViewList.js
@@ -59,8 +59,8 @@ export default function CandidateViewList(view, sim, screen, election) {
         const fillUndefined = Array(nk).fill(undefined)
         self.setCandidateWins(fillUndefined)
     }
-    self.updateXY = () => {
-        canViews.forEach((canView) => canView.candidate.updateXY())
+    self.updateViewXY = () => {
+        canViews.forEach((canView) => canView.graphic.updateViewXY())
     }
 
     // Render //

src/castVotes/castRankingFindPolygons.js
@@ -92,9 +92,8 @@ export default function castRankingFindPolygons(voterGeom, canGeoms) {
  * @param {Object} voterGeom
  */
 function makeCircle(voterGeom) {
-    const r = voterGeom.w / 2
-    const { x } = voterGeom
-    const { y } = voterGeom
+    const { x, y, w } = voterGeom
+    const r = w / 2
     const n = 100
     const circle = Array(n)
     for (let i = 0; i < n; i++) {

src/tooltips/tooltipBox.js
@@ -1,22 +1,23 @@
 import hideOnClickOutside from './hideOnClickOutside.js'
 
-export default function tooltipBox(entity, screen) {
+export default function tooltipBox(graphic, screen) {
     const box = document.createElement('div')
 
     box.className = 'tooltipBox'
     box.style.position = 'absolute'
 
     function update() {
-        if (entity.x > screen.width * 0.5) {
-            box.style.right = `${screen.width - entity.x}px`
+        const { x, y } = graphic
+        if (x > screen.width * 0.5) {
+            box.style.right = `${screen.width - x}px`
             box.style.left = 'unset'
         } else {
-            box.style.left = `${entity.x}px`
+            box.style.left = `${x}px`
             box.style.right = 'unset'
         }
-        box.style.top = `${entity.y}px`
+        box.style.top = `${y}px`
     }
-    update(entity)
+    update()
     // box.style.width = '10px'
     // box.style.height = '10px'
     box.style.border = (screen.darkMode) ? '1px solid white' : '1px solid black'

commit 93ed84550785e1f754c2b0766439cce31ed8962c

Fixed bug introduced 3 days ago in 097e4ca. Undo wasn't triggering a view update for entities. Now, the changes are propagated to View.

src/sim/states/ViewOne.js
@@ -88,6 +88,12 @@ export default function ViewOne(entities, screen, menu, changes, sim, view) {
     self.update = (electionResults) => {
         if (changes.checkNone()) return
 
+        if (changes.check(['draggables'])) {
+            // this will trigger when undo moves entities
+            voterViewList.updateViewXY()
+            candidateViewList.updateViewXY()
+        }
+
         vizOne.update(electionResults)
         vizExplanation.update(electionResults)
         self.testVoteView()

src/sim/states/ViewSample.js
@@ -64,6 +64,12 @@ export default function ViewSample(entities, screen, menu, changes, sim, view) {
         // The election handles any changes.
         // The electionResults communicates how to visualize the election.
 
+        if (changes.check(['draggables'])) {
+            // this will trigger when undo moves entities
+            voterViewList.updateViewXY()
+            candidateDnViewList.updateViewXY()
+        }
+
         vizSample.update(addResult)
 
         const { pointsChanged } = addResult