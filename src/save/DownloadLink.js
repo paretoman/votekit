485f8dd60ce7d4fe36a0caeb4bd7e428375b0638
paretoman <paretoman1@gmail.com>
2023-01-15 09:16:39 -0500

    add button to hide download Link.

--- src/save/DownloadLink.js
+++ src/save/DownloadLink.js
@@ -1,17 +1,27 @@
 export default function DownloadLink(text) {
     const self = this
 
+    self.div = document.createElement('div')
+
     const downloadLink = document.createElement('a')
     downloadLink.innerText = text
     downloadLink.download = text
     downloadLink.style.margin = '4px'
-    self.div = downloadLink
+    self.div.append(downloadLink)
+
+    const hideLink = document.createElement('button')
+    hideLink.className = 'button2'
+    hideLink.innerText = 'X'
+    hideLink.onclick = () => { self.hide() }
+    self.div.append(hideLink)
 
     self.show = () => {
         downloadLink.hidden = false
+        hideLink.hidden = false
     }
     self.hide = () => {
         downloadLink.hidden = true
+        hideLink.hidden = true
     }
     self.setUrl = (url) => {
         downloadLink.href = url
