paretoman/votekit

commit cb63ec149cbbcba6a8d9377437218389edbc6cc5

normalize tallyFraction inside party for standardization sake.

src/election/socialChoiceMethods/olprA.js
@@ -54,9 +54,11 @@ export default function olprA({ votes, socialChoiceOptions }) {
         const socialChoiceOptions2 = { seats: partyAllocation[i] }
         const cansInParty = range(numCans).filter((k) => partiesByCan[k] === i)
         const tfWithinParty = cansInParty.map((k) => tallyFractions[k])
+        const totalTFInParty = tfWithinParty.reduce((p, c) => p + c)
+        const fractionTfWithinParty = tfWithinParty.map((x) => x / totalTFInParty)
         // Run sntv.
         const socialChoiceInParty = sntv({
-            votes: { tallyFractions: tfWithinParty },
+            votes: { tallyFractions: fractionTfWithinParty },
             socialChoiceOptions: socialChoiceOptions2,
         })
         const allocationInParty = socialChoiceInParty.allocation

commit 39f1a627d693bc584e807ed054f189da71c48483

fix olpr bug introduced yesterday in commit cb63ec1

src/election/socialChoiceMethods/olprA.js
@@ -54,7 +54,7 @@ export default function olprA({ votes, socialChoiceOptions }) {
         const socialChoiceOptions2 = { seats: partyAllocation[i] }
         const cansInParty = range(numCans).filter((k) => partiesByCan[k] === i)
         const tfWithinParty = cansInParty.map((k) => tallyFractions[k])
-        const totalTFInParty = tfWithinParty.reduce((p, c) => p + c)
+        const totalTFInParty = tfWithinParty.reduce((p, c) => p + c, 0)
         const fractionTfWithinParty = tfWithinParty.map((x) => x / totalTFInParty)
         // Run sntv.
         const socialChoiceInParty = sntv({