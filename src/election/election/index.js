785c73bac55134c450ee415953f8b8f1f98fca68
paretoman <paretoman1@gmail.com>
2023-02-11 13:17:23 -0500

    index.js: made package index for votekit-election

---
+++ src/election/election/index.js
@@ -0,0 +1,38 @@
+import createDistrictGeometry, { makeTracts, updateCensus, updateDistricts, updateVoters } from '../districtGeometry/createDistrictGeometry.js'
+import CandidateDistributionSampler1D from '../sampler/CandidateDistributionSampler1D.js'
+import CandidateDistributionSampler2D from '../sampler/CandidateDistributionSampler2D.js'
+import ElectionSampler from '../sampler/ElectionSampler.js'
+import socialChoiceMethods, { socialChoiceMethodMetadataByFunctionName } from '../socialChoiceMethods/socialChoiceMethods.js'
+import getCanBorders from '../voteCasters/voteCasters/getCanBorders.js'
+import voteCasters from '../voteCasters/voteCasters/voteCasters.js'
+import election from './election.js'
+
+const geography = {
+    createDistrictGeometry,
+    makeTracts,
+    updateCensus,
+    updateDistricts,
+    updateVoters,
+}
+const voting = {
+    getCanBorders,
+    voteCasters,
+}
+const socialChoice = {
+    socialChoiceMethods,
+    socialChoiceMethodMetadataByFunctionName,
+}
+const sampler = {
+    CandidateDistributionSampler1D,
+    CandidateDistributionSampler2D,
+    ElectionSampler,
+}
+const electionIndex = {
+    geography,
+    voting,
+    socialChoice,
+    election,
+    sampler,
+}
+
+export default electionIndex
