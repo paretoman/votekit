881e80a81b681c5eb7dd6be56ec6cf15b4f9ed9c
paretoman <paretoman1@gmail.com>
2023-02-15 18:43:01 -0500

    don't randomize seed if we just loaded a seed.

--- src/sim/modes/updateSeeds.js
+++ src/sim/modes/updateSeeds.js
@@ -8,6 +8,7 @@ import seedrandom from '../../election/src/lib/snowpack/build/snowpack/pkg/seedr
 export default function updateSeeds(simOptions, changes) {
     if (!changes.checkAny()) return
     if (!simOptions.seedRandom) return
+    if (changes.check(['init'])) return
 
     const rng = seedrandom()
     const seed = Math.floor(rng() * 10000)
