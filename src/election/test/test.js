a5bdf04816205cee1ac39f9f969d4d39d3e0666e
paretoman <paretoman1@gmail.com>
2023-02-15 22:23:43 -0500

    lint

--- src/election/test/test.js
+++ src/election/test/test.js
@@ -12,7 +12,7 @@ describe('The election', () => {
 describe('Sampling an election', () => {
     it('should sample the same winner each time', () => {
         const point0 = testElectionSample()
-        const expectedPoint0 = [-49.78348432747873, 4.589134481725754,]
+        const expectedPoint0 = [-49.78348432747873, 4.589134481725754]
         assert.deepEqual(expectedPoint0, point0)
     })
 })
