4e3ca769ead075466f335b09052b4b3ae463440e
paretoman <paretoman1@gmail.com>
2023-02-16 22:18:41 -0500

    jsdocs: works! Uses namespace!

--- src/election/src/voteCasters/voteCasters/typesVC.js
+++ src/election/src/voteCasters/voteCasters/typesVC.js
@@ -1,13 +1,15 @@
 // How exporting types works:
 // https://stackoverflow.com/a/73232942
-
+/**
+ * @namespace typesVC
+ */
 /**
  * @typedef {Object} geometry - geometry for casting votes
  * @property {Object[]} canGeoms - For 2D, an array of objects: {x,y}. For 1D, an array of objects: {x}.
  * @property {Object[]} voterGeoms - For 2D, an array of objects: {x,y,w}. For 1D, an array of objects: {x,w,densityProfile}.
  * @property {Number} dimensions
  * @property {Object} parties
- * @memberof typdefs
+ * @memberof typesVC
  */
 
 export default {}
