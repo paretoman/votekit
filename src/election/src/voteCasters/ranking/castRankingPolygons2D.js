fd4381ca32553ecf1c68dd991a396de69c46927a
paretoman <paretoman1@gmail.com>
2023-02-23 08:25:20 -0500

    reduce verbosity for castRankingPolygons2D

--- src/election/src/voteCasters/ranking/castRankingPolygons2D.js
+++ src/election/src/voteCasters/ranking/castRankingPolygons2D.js
@@ -6,15 +6,16 @@ import castRankingFindPolygons from './castRankingFindPolygons.js'
 /**
  * Sum area of voter distributions to tally the votes.
  */
-export default function castRankingPolygons2D(voterGeom, geometry) {
+export default function castRankingPolygons2D(voterGeom, geometry, castOptions) {
     const { canPoints } = geometry
+    const { verbosity } = castOptions
 
     // draw lines across shape of voterGeom
     let { cells, rankings, cansByRankList } = castRankingFindPolygons(voterGeom, canPoints)
 
     // find area of polygons
     const cn = cells.length
-    let areas = Array(cn)
+    const areas = Array(cn)
     let totalArea = 0
     for (let i = 0; i < cn; i++) {
         // return area for each ranking
@@ -26,13 +27,14 @@ export default function castRankingPolygons2D(voterGeom, geometry) {
     // sometimes near-zero-area polygons are formed that need to be removed
     // because they also have rankings that don't make sense.
     const tol = 0.000001
-    rankings = rankings.filter((_, i) => Math.abs(areas[i]) > tol)
     cansByRankList = cansByRankList.filter((_, i) => Math.abs(areas[i]) > tol)
-    cells = cells.filter((_, i) => Math.abs(areas[i]) > tol)
-    areas = areas.filter((a) => Math.abs(a) > tol)
+    const voteCounts = areas.filter((a) => Math.abs(a) > tol)
 
-    const [voteCounts, totalVotes] = [areas, totalArea]
-    return {
-        rankings, cansByRankList, voteCounts, totalVotes, cells,
+    const totalVotes = totalArea
+    if (verbosity < 2) {
+        return { voteCounts, totalVotes, cansByRankList }
     }
+    rankings = rankings.filter((_, i) => Math.abs(areas[i]) > tol)
+    cells = cells.filter((_, i) => Math.abs(areas[i]) > tol)
+    return { voteCounts, totalVotes, cansByRankList, rankings, cells }
 }
