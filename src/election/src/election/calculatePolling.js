0314a50ceaf5fc1b3a34082d00091cac5c51930a
paretoman <paretoman1@gmail.com>
2023-03-09 22:13:35 -0500

    polling only for score for now

--- src/election/src/election/calculatePolling.js
+++ src/election/src/election/calculatePolling.js
@@ -4,7 +4,13 @@ export default function calculatePolling(electionResultsList) {
     if (electionResultsList.length === 0) return polling
 
     const lastElectionResults = electionResultsList[electionResultsList.length - 1]
+    if (lastElectionResults.votes.candidateTallies === undefined) {
+        return polling
+    }
     const { scoreFractionAverageByCan } = lastElectionResults.votes.candidateTallies
+    if (scoreFractionAverageByCan === undefined) {
+        return polling
+    }
 
     const highestScore = Math.max(...scoreFractionAverageByCan)
     polling.highestScore = highestScore
