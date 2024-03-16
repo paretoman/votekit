041201073a0fb16adee540a569af479255ee457c
paretoman <paretoman1@gmail.com>
2024-03-16 08:15:06 -0400

    sim package. imports/exports.

---
+++ src/sim/sim-index.js
@@ -0,0 +1,13 @@
+import Sim from './sim/Sim.js'
+import Changes from './sim/Changes.js'
+import ComMessenger from './command/ComMessenger.js'
+import getTestGeometry from './geometry/getTestGeometry.js'
+import checkSomeStrategy from './geometry/checkSomeStrategy.js'
+
+export {
+    Sim,
+    Changes,
+    ComMessenger,
+    getTestGeometry,
+    checkSomeStrategy,
+}
