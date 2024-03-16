21704fb228b8d9339c6c86ed5c15fe3f36f72a0f
paretoman <paretoman1@gmail.com>
2024-03-16 11:56:22 -0400

    fix tests. fix bug from months ago when I introduced phases.

--- src/compute/electionSequence/electionGeneral.js
+++ src/compute/electionSequence/electionGeneral.js
@@ -19,6 +19,7 @@ export default function electionGeneral(geometry, optionsBag) {
         phases: {
             general,
         },
+        phaseNames: ['general'],
         geometry,
         optionsBag,
         socialChoiceResults,
