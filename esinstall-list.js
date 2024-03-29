e3b574dfac44408631d674140421dc939b25ed12
paretoman <paretoman1@gmail.com>
2024-03-29 18:35:53 -0400

    esinstall. use just esinstall, which is the part of snowpack that I was using

---
+++ esinstall-list.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { install } from 'esinstall'
+
+await install([
+    // compute
+    'd3-delaunay',
+    'd3-polygon',
+    'simplex-noise',
+    'seedrandom',
+
+    // all
+    '@tweenjs/tween.js',
+    '@mithrandirii/canvas2svg',
+    'd3-contour',
+    'd3-geo',
+
+])
